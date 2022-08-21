e7cc934a0749be1b76ecb4ad9e04e053e1f5b63f
Justin Cueva <justincueva@Justins-MBP.lan>
2022-08-21 14:08:34 -0700

    adjusted the measurements of the inputs

--- ui.py
+++ ui.py
@@ -4,7 +4,7 @@ import tkinter
 class UserInterface:
     def __init__(self):
         self.window = tkinter.Tk()
-        self.window.config(padx=20, pady=20)
+        self.window.config(padx=50, pady=50)
         self.window.title("Pass Man GUI")
 
         # LOGO IMAGE
@@ -14,23 +14,24 @@ class UserInterface:
         self.logo = self.canvas.create_image(100, 100, image=logo_image)
 
         # LABELS
-        self.website_label = tkinter.Label(text="Website:")
+        label_width = 15
+        self.website_label = tkinter.Label(text="Website:", width=label_width)
         self.website_label.grid(row=1, column=0)
-        self.email_username_label = tkinter.Label(text="Email/Username:")
+        self.email_username_label = tkinter.Label(text="Email/Username:", width=label_width)
         self.email_username_label.grid(row=2, column=0)
-        self.password_label = tkinter.Label(text="Password:")
+        self.password_label = tkinter.Label(text="Password:", width=label_width)
         self.password_label.grid(row=3, column=0)
 
         # INPUTS
-        self.website_input = tkinter.Entry(width=35)
-        self.website_input.grid(row=1, column=1, columnspan=2)
-        self.email_username_input = tkinter.Entry(width=35)
-        self.email_username_input.grid(row=2, column=1, columnspan=2)
-        self.password_input = tkinter.Entry(width=21)
+        self.website_input = tkinter.Entry(width=30)
+        self.website_input.grid(row=1, column=1, columnspan=2, sticky="ew")
+        self.email_username_input = tkinter.Entry(width=30)
+        self.email_username_input.grid(row=2, column=1, columnspan=2, sticky="ew")
+        self.password_input = tkinter.Entry()
         self.password_input.grid(row=3, column=1, columnspan=1, sticky="ew")
 
         # BUTTONS
-        self.generate_password_button = tkinter.Button(width=10, text="Generate Password", font=("Arial", 10, "normal"))
+        self.generate_password_button = tkinter.Button(text="Generate Password", font=("Arial", 12, "normal"))
         self.generate_password_button.grid(row=3, column=2, sticky="nesw")
         self.add_button = tkinter.Button(text="Add")
         self.add_button.grid(row=4, column=1, columnspan=2, sticky="nesw")
