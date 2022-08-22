04419fc728bd49e89365f55c995cf1027c3a289e
Justin Cueva <justincueva@Justins-MBP.lan>
2022-08-22 02:59:28 -0700

    added functionality to gen pass button

--- ui.py
+++ ui.py
@@ -1,3 +1,4 @@
+import random
 import tkinter
 from tkinter import messagebox
 
@@ -34,12 +35,43 @@ class UserInterface:
         self.password_input.grid(row=3, column=1, columnspan=1, sticky="ew")
 
         # BUTTONS
-        self.generate_password_button = tkinter.Button(text="Generate Password", font=("Arial", 12, "normal"))
+        self.generate_password_button = tkinter.Button(text="Generate Password", command=self.generate_password,font=("Arial", 12, "normal"))
         self.generate_password_button.grid(row=3, column=2, sticky="nesw")
         self.add_button = tkinter.Button(text="Add", command=self.save)
         self.add_button.grid(row=4, column=1, columnspan=2, sticky="nesw")
 
         self.window.mainloop()
+
+    def generate_password(self):
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                   'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                   'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+        nr_letters = random.randint(8, 10)
+        nr_symbols = random.randint(2, 4)
+        nr_numbers = random.randint(2, 4)
+
+        password_list = []
+
+        for char in range(nr_letters):
+            password_list.append(random.choice(letters))
+
+        for char in range(nr_symbols):
+            password_list += random.choice(symbols)
+
+        for char in range(nr_numbers):
+            password_list += random.choice(numbers)
+
+        random.shuffle(password_list)
+
+        password = ""
+        for char in password_list:
+            password += char
+
+        # self.password_input.delete(0, tkinter.END)
+        # self.password_input.insert(0, password)
 
     def save(self):
         website = self.website_input.get()
